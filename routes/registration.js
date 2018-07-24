c809f52e2ccd43028648614fb49ed4bf147698b0
Roma Kutischev <rukkola2@gmail.com>
2018-07-24 13:23:19 +0300

    changes

--- routes/registration.js
+++ routes/registration.js
@@ -3,49 +3,57 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
-const emailTablePath = path.join(__dirname, '../public/database/emails.json');
-let emails;
+var dbHandler = require('../modules/dbHandler');
 
-if (!fs.existsSync(emailTablePath)) {
-  fs.writeFileSync(emailTablePath, '[]');
-}
+const usersTablePath = path.join(__dirname, '../public/database/users.json'); 
+let users;
 
-emails = JSON.parse(fs.readFileSync(emailTablePath));
+dbHandler.checkJSONexistence(usersTablePath);
 
-function validation(email, password, confirmPass) {
+users = JSON.parse(fs.readFileSync(usersTablePath));
+
+function validate(email, password, confirmPass) {
   let errors = [];
   const regEx1 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const regEx2 = /[0-9]/;
   const regEx3 = /[a-z]/;
 
-  String(email).toLowerCase().match(regEx1) ? true : errors.push('Invalid email');
-  password === confirmPass ? true : errors.push("Password doesn't match confirmation");
-  String(password).length > 7 ? true : errors.push("Password must be longer");
-  String(password).match(regEx2) ? true : errors.push('Password must contain numbers too');
-  String(password).toLowerCase().match(regEx3) ? true : errors.push('Password must contain letters too')
+  String(email).toLowerCase().match(regEx1) ? true : errors.push({ message: 'Invalid email' });
+
+  if (password !== confirmPass) {
+    errors.push({ message: "Password doesn't match confirmation" });
+    return { isValid: false, errors };
+  } 
+  
+  String(password).length > 7 ? true : errors.push({ message: "Password must be longer" });
+  String(password).match(regEx2) ? true : errors.push({ message: 'Password must contain numbers too' });
+  String(password).toLowerCase().match(regEx3) ? true : errors.push({ message: 'Password must contain letters too' })
+  
   if (errors.length < 1) {
-    return 'ok';
-  } else {
-    return errors;
+    return { isValid: true, errors };
   }
+
+  return { isValid: false, errors };
 }
 
 router.post('/', (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const confirmPass = req.body.confirmPass;
+  const {email, password, confirmPass} = req.body;
 
-  if (emails.includes(req.body.email)) {
-    return res.send('Email is registrated already');    
-  } else {
-    const valid = validation(email, password, confirmPass);
-    if (valid == 'ok') {
-      emails.push({ email: email, password: password });
-      return res.send({ email: email, password: password });
-    } else {
-      return res.send(valid.toString());
-    }
+  if (users.find(user => user.email === email)) {
+    return res.status(400).send({message: 'Email is registrated already'});  
   }
+
+  const valid = validate(email, password, confirmPass);
+  if (valid.isValid !== true) {
+    console.log(valid.errors)
+    return res.status(400).send(valid.errors);
+  }
+
+  const id = dbHandler.setId(users);
+  users.push({ id, email, password });
+
+  fs.writeFileSync(usersTablePath, JSON.stringify(users));
+  return res.status(201).send({ id, email });
 });
 
 module.exports = router;
