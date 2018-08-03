c741226c473a710a94a6edf79c4c5d47cca6ea22
Roma Kutischev <rukkola2@gmail.com>
2018-08-03 12:30:44 +0300

    fixed login route

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,7 @@ var uidgen = new UIDGenerator();
 var dbHandler = require("../modules/dbHandler");
 
 const usersTablePath = path.join(__dirname, "../public/database/users.json");
-const takensTablePath = path.join(__dirname, "../public/database/tokens.json")
+const tokensTablePath = path.join(__dirname, "../public/database/tokens.json")
 let users;
 
 dbHandler.checkJSONexistence(usersTablePath);
@@ -19,18 +19,21 @@ users = JSON.parse(fs.readFileSync(usersTablePath));
 router.post('/', (req, res, next) => {
   const{ email, password } = req.body;
   const userIndex = users.findIndex(user => user.email === email);
-  const userId = users[userIndex].id;
-  const expires = 2592000000 + Date.now();
 
   if (userIndex === -1 || users[userIndex].password != password) {
     return res.status(401).send([{ message: "Wrong Email or password" }]);
   }
-  
+
+  const userId = users[userIndex].id;
+  const expires = 2592000000 + Date.now();  
   const token = uidgen.generateSync();
-  dbHandler.checkJSONexistence(takensTablePath);
-  fs.writeFileSync(takensTablePath, JSON.stringify({ token, expires, userId }))
-  res.status(200).cookie("token", token, { maxAge: expires }).send("cookie set");
-  
+
+  dbHandler.checkJSONexistence(tokensTablePath);
+
+  let tokensTable = JSON.parse(fs.readFileSync(tokensTablePath));
+  tokensTable.push({ token, expires: new Date(expires).toJSON(), userId });
+  fs.writeFileSync(tokensTablePath, JSON.stringify(tokensTable));
+  res.status(200).cookie("token", token, { maxAge: expires }).send("cookie set");  
 });
 
 module.exports = router;
