e7ef039e659b571b083017e1f8cc89f3f313b3a3
Roma Kutischev <rukkola2@gmail.com>
2018-08-06 22:04:01 +0300

    changes

--- routes/login.js
+++ routes/login.js
@@ -5,35 +5,34 @@ var path = require("path");
 var UIDGenerator = require("uid-generator");
 var uidgen = new UIDGenerator();
 
-
 var dbHandler = require("../modules/dbHandler");
 
 const usersTablePath = path.join(__dirname, "../public/database/users.json");
-const tokensTablePath = path.join(__dirname, "../public/database/tokens.json")
-let users;
-
+const tokensTablePath = path.join(__dirname, "../public/database/tokens.json");
 dbHandler.checkJSONexistence(usersTablePath);
+dbHandler.checkJSONexistence(tokensTablePath);
 
-users = JSON.parse(fs.readFileSync(usersTablePath));
+let users = JSON.parse(fs.readFileSync(usersTablePath));
+let tokensTable = JSON.parse(fs.readFileSync(tokensTablePath));
 
-router.post('/', (req, res, next) => {
-  const{ email, password } = req.body;
+router.post("/", (req, res, next) => {
+  const { email, password } = req.body;
   const userIndex = users.findIndex(user => user.email === email);
 
-  if (userIndex === -1 || users[userIndex].password != password) {
+  if (userIndex === -1 || users[userIndex].password !== password) {
     return res.status(401).send([{ message: "Wrong Email or password" }]);
   }
 
   const userId = users[userIndex].id;
-  const expires = 2592000000 + Date.now();  
+  const expires = new Date(2592000000 + Date.now());
   const token = uidgen.generateSync();
 
-  dbHandler.checkJSONexistence(tokensTablePath);
-
-  let tokensTable = JSON.parse(fs.readFileSync(tokensTablePath));
-  tokensTable.push({ token, expires: new Date(expires).toJSON(), userId });
+  tokensTable.push({ token, expires, userId });
   fs.writeFileSync(tokensTablePath, JSON.stringify(tokensTable));
-  res.status(200).cookie("token", token, { maxAge: expires }).send("cookie set");  
+  return res
+    .status(201)
+    .cookie("token", token, { expires, httpOnly: true })
+    .send({});
 });
 
 module.exports = router;
--- routes/registration.js
+++ routes/registration.js
@@ -40,7 +40,7 @@ router.post('/', (req, res, next) => {
   const { email, password, confirmPass } = req.body;
 
   if (users.find(user => user.email === email)) {
-    return res.status(400).send({message: 'Email is registrated already'});  
+    return res.status(400).send([{message: 'Email is registrated already'}]);
   }
 
   const valid = validate(email, password, confirmPass);
